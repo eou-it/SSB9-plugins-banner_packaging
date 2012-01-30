6edc80ca2da3eca5aeb82f5aa631918bc82cb877
chasdev <chasdev@me.com>
2012-01-30 09:26:50 -0500

    Support use in-place

--- scripts/PackageRelease.groovy
+++ scripts/PackageRelease.groovy
@@ -1,4 +1,4 @@
-/*********************************************************************************
+/* *******************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
  This copyrighted software contains confidential and proprietary information of 
  SunGard Higher Education and its subsidiaries. Any use of this software is limited 
@@ -11,6 +11,7 @@
  **********************************************************************************/
 
 import org.apache.ivy.core.report.ArtifactDownloadReport
+import org.codehaus.groovy.grails.plugins.GrailsPluginUtils
 import org.codehaus.groovy.grails.resolve.IvyDependencyManager
 
 
@@ -23,98 +24,104 @@ import org.codehaus.groovy.grails.resolve.IvyDependencyManager
 scriptEnv = "production"
 includeTargets << grailsScript( "_GrailsPackage" )
 includeTargets << grailsScript( "_GrailsWar" )
+includeTargets << grailsScript( "_GrailsEvents" )
 
 
 target( default:"Package Release" ) {
-	depends( checkVersion, compile, createConfig, genReleaseProperties, war )
-    
-    event( "PackageReleaseStart", [] )
 
-	File templateZip = getTemplateHomeZip()
+    depends( checkVersion, compile, createConfig, genReleaseProperties, war )
+    pluginName = "banner-packaging"
+    event "PackageReleaseStart", []
+
+    try {
+        verifyCleanWorkingCopy( basedir )
+    } 
+    catch (e) {
+        // TODO: Prompt user unless --non-interactive was specified, to allow
+        // someone to proceed even though the git working directory is not clean.
+        println "*****************************************************"
+        println "<<<<<<<<<< Git Working Copy NOT Clean >>>>>>>>>>>>>>>"
+        println "(proceeding anyway (temporarily, until a prompt is added to provide a choice)"
+        // bury the exception so we can proceed...
+    }
+
+    // We'll fire a 'TemplateZip' event, and respond to it via the _Events.groovy script
+    // before continuing. The 'eventTemplateZip' handler will use Ivy to retrieve all of 
+    // the dependencies that need to be included in the release package
+    //
+    event "TemplateZip", [pluginName, '1.0.2'] // TODO: Read plugin version from it's *Plugin file...
+
     File releasePackageZip = new File( "${basedir}/target/release-${metadata.'app.name'}-${metadata.'app.version'}.zip" )
     ant.delete( file:releasePackageZip )
 
-	def stagingDir = new File( "${projectWorkDir}/installer-staging" )
-	ant.delete( dir:stagingDir )
-	ant.mkdir( dir:stagingDir )
-	ant.unzip( src:templateZip, dest:stagingDir ) {
-	    patternset {
-	        exclude( name:"installer/apache-ant-1.8.2/docs/**/*" )
-	    }
-	}
-	
-	File customDir = new File( "${basedir}/src/installer" )	
-	if (customDir.exists()) {
-		ant.copy( todir:"${stagingDir}/installer", overwrite:true ) {
-			fileset( dir:"${customDir}", includes:"**/*" )
-		}
-	}
-	
-	ant.mkdir( dir:"${stagingDir}/i18n" )
-	ant.copy( todir:"${stagingDir}/i18n" ) {
-		fileset( dir:"${basedir}/grails-app/i18n", includes:"**/*" )
-		// we'll also copy the release.properties to the i18n directory so that 
-		// it is easily accessible by the installer...
-		fileset( dir: "$basedir/target/classes", includes: "release.properties" )
-	}
-	
-	ant.mkdir( dir:"${stagingDir}/webapp" )
-	ant.copy( todir:"${stagingDir}/webapp" ) {
-		fileset( dir:"${basedir}/target", includes:"*.war" )
-	}
-	
-	ant.mkdir( dir:"${stagingDir}/config" )
-	ant.copy( todir:"${stagingDir}/config" ) {
-		fileset( dir:"${basedir}/", includes:"*_configuration.example" )
-	}
-	
-	ant.mkdir( dir:"${stagingDir}/lib" )
-	ant.copy( todir:"${stagingDir}/lib" ) {
-		fileset( dir:"${basedir}/target", includes:"ojdbc6.jar" )
-		fileset( dir:"${basedir}/src", includes:"logging.properties" )
-	}
+    def stagingDir = new File( "${projectWorkDir}/installer-staging" )
+    ant.delete( dir:stagingDir )
+    ant.mkdir( dir:stagingDir )
 
-	ant.zip( destfile:releasePackageZip ) {
-		fileset( dir:stagingDir )
-	}
+    File templateZip = getTemplateHomeZip()
+    ant.unzip( src:templateZip, dest:stagingDir ) {
+        patternset {
+            exclude( name:"installer/apache-ant-1.8.2/docs/**/*" )
+        }
+    }
+
+    File customDir = new File( "${basedir}/src/installer" )	
+    if (customDir.exists()) {
+        ant.copy( todir:"${stagingDir}/installer", overwrite:true ) {
+            fileset( dir:"${customDir}", includes:"**/*" )
+        }
+    }
+
+    ant.mkdir( dir:"${stagingDir}/i18n" )
+    ant.copy( todir:"${stagingDir}/i18n" ) {
+        fileset( dir:"${basedir}/grails-app/i18n", includes:"**/*" )
+        // we'll also copy the release.properties to the i18n directory so that 
+        // it is easily accessible by the installer...
+        fileset( dir: "$basedir/target/classes", includes: "release.properties" )
+    }
+
+    ant.mkdir( dir:"${stagingDir}/webapp" )
+    ant.copy( todir:"${stagingDir}/webapp" ) {
+        fileset( dir:"${basedir}/target", includes:"*.war" )
+    }
+
+    ant.mkdir( dir:"${stagingDir}/config" )
+    ant.copy( todir:"${stagingDir}/config" ) {
+        fileset( dir:"${basedir}/", includes:"*_configuration.example" )
+    }
+
+    ant.mkdir( dir:"${stagingDir}/lib" )
+    ant.copy( todir:"${stagingDir}/lib" ) {
+        fileset( dir:"${basedir}/target", includes:"ojdbc6.jar" )
+        fileset( dir:"${basedir}/src", includes:"logging.properties" )
+    }
+
+    ant.zip( destfile:releasePackageZip ) {
+        fileset( dir:stagingDir, excludes:"*.lock" )
+    }
 
     ant.delete( dir:stagingDir )
     event( "PackageReleaseEnd", [] )
 }
 
 
-private File getTemplateHomeZip() {
-    
-	//The template home dir is a zipped distro within the plugin directory.  We have to find the plugin dir.
-	File zip = null
-	pluginSettings.getPluginInfos().each() {
-		if (it.name.equals( "banner-packaging" )) {
-			zip = new File( it.pluginDir.getFile(), "template.zip" )
-		}
-	}
-	if (zip == null) {
-		throw new Exception( "Template home zip could not be found" )
-	}
-	zip
-}
-
 target( genReleaseProperties: "Creates a release.properties file holding a newly assigned build number and the application version." ) {    
-    
-	// This target uses a 'build number' web service to retrieve the next build number 
+
+    // This target uses a 'build number' web service to retrieve the next build number 
     // for the project.  Each project is assigned a UUID (manually), and this UUID is 
     // supplied to the web service to identify which 'build number sequence' to increment 
     // and return. The project's UUID, and the URL to the service, are 
     // found in the project's configuration. 
-    // 
+    //
     def appName    = "${metadata.'app.name'}"
     def appVersion = "${metadata.'app.version'}"
-    
+
     // def config = new ConfigSlurper().parse( new File( "${basedir}/grails-app/conf/Config.groovy" ).toURL() )
     def uuid = config.build.number.uuid
-    def url  = config.build.number.base.url + uuid 
-    
+    def url  = config.build.number.base.url + uuid
     def extractedBuildNumber = "Unassigned" // used when the configuration does not specify a uuid 
-    if (uuid instanceof String && url instanceof String) { 
+
+    if (uuid instanceof String && url instanceof String) {
         try {
             def buildNumberProperty  = url.toURL().getText()
             extractedBuildNumber = (buildNumberProperty =~ /[\d]+/).collect { it }[0]
@@ -125,14 +132,14 @@ target( genReleaseProperties: "Creates a release.properties file holding a newly
             // we'll bury the excepton and let the packaging proceed...
         }
     }
-    
-    def scmRevision   = "git rev-parse HEAD".execute().text    
+
+    def scmRevision   = "git rev-parse HEAD".execute().text
     def scmRepository = "git config --get remote.origin.url".execute().text
-    
-    def content = """#This file is automatically generated and contains release specific properties.  
-                     |#This file MUST not be changed. 
+
+    def content = """|#This file is automatically generated and contains release specific properties.
+                     |#This file MUST not be changed.
                      |#
-                     |#  ****** DO NOT EDIT OR TRANSLATE THIS FILE. ******* 
+                     |#  ****** DO NOT EDIT OR TRANSLATE THIS FILE. *******
                      |#
                      |#******************************************************
                      |#*             Version and Build Number               *
@@ -140,20 +147,138 @@ target( genReleaseProperties: "Creates a release.properties file holding a newly
                      |
                      |application.name=$appName
                      |application.version=$appVersion
-					 |application.build.number=$extractedBuildNumber
+                     |application.build.number=$extractedBuildNumber
                      |application.build.time='${new Date()}'
+                     |
                      |
                      |#******************************************************
                      |#*               Source Code Revision                 *
                      |#**************************************************** */
                      |
+                     |# The following source code repository and revision
+                     |# values refer to the original development repository.
+                     |#
+                     |# Clients who decide to use git to manage their source
+                     |# code modifications may use the bare repository clone
+                     |# included within the release package.
                      |git.repository=$scmRepository
                      |git.commit=$scmRevision
                      |
+                     |
+                     |#******************************************************
+                     |#*                 Plugin Dependencies                *
+                     |#**************************************************** */
+                     |
                      |""".stripMargin()
-    
-    def releasePropertiesFile = new File( "$basedir/target/classes/release.properties" )    
+
+    def pluginVersionInfo = retrievePluginInfo()
+    pluginVersionInfo.each {
+        content += "\n# Plugin: ${it.key}"
+        content += "\n#     version:   ${it.value.version}"
+        content += "\n#     location:  ${it.value.location}"
+
+        if (it.value.treeish) content += "\n#     Git treeish: ${it.value.treeish}"
+        if (it.value.branch)  content += "\n#     Git branch:  ${it.value.branch}"
+        if (it.value.status)  content += "\n#     Git status:  ${it.value.status}"
+        else                  content += "\n"
+    }
+    def releasePropertiesFile = new File( "$basedir/target/classes/release.properties" )
     releasePropertiesFile.write content
+}
+
+
+// ------------------------------- Private Methods -----------------------------
+
+
+private File getTemplateHomeZip() {
+
+    //The template home dir is a zipped distro within the plugin directory.  We have to find the plugin dir.
+    File zip = null
+    pluginSettings.getPluginInfos().each() {
+        if (it.name.equals( "banner-packaging" )) {
+            zip = new File( it.pluginDir.getFile(), "template.zip" )
+        }
+    }
+
+    if (zip == null) {
+        throw new Exception( "Template home zip could not be found" )
+    }
+    zip
+}
+
+
+/**
+ * Records plugin versions, and for in-place plugins the Git SHA1.
+ **/
+private Map retrievePluginInfo() {
+
+    def inlinePluginVersionInfo = [:]
+    def gitSubmodulesFile = new File( "${projectWorkDir}/.gitmodules" )
+
+    GrailsPluginUtils.getPluginInfos().each {
+        def inlinePluginDirPaths = pluginSettings.inlinePluginDirectories*.file.path
+        inlinePluginDirPaths.each { println "INLINE: ${it}" }
+
+        def pluginDirPath = it.pluginDir.path
+        def sha1   = ''
+        def branch = ''
+        def status = ''
+        if (pluginDirPath in inlinePluginDirPaths) {
+            sha1   = new File("${it.pluginDir.path}/.git/refs/heads/master").text
+            branch = getWorkingBranch( pluginDirPath )
+            status = getStatus( pluginDirPath )
+        }
+        def versionInfo = [ version:  "${it.version}",
+                            location: "${it.pluginDir.path}",
+                            treeish:  "${sha1}",
+                            branch:   "${branch}",
+                            status:   "${status}" ]
+        inlinePluginVersionInfo[it.name] = versionInfo
+    }
+//    inlinePluginVersionInfo.each { println "${it.key} : ${it.value}"}
+    inlinePluginVersionInfo
+}
+
+
+private String getWorkingBranch( dir ) {
+
+    def currentBranch = ''
+    matcher = ~/\* (.*)\s/
+
+    process = "git branch".execute( null, new File( dir ) )
+    process.in.eachLine { line ->
+        m = line =~ /\*\s+(.*)\s?/
+        if (m) {
+            currentBranch = m[0][1]
+            return
+        }
+    }
+    currentBranch
+}
+
+
+private verifyCleanWorkingCopy(dir) {
+
+    if (getStatus(dir).value.status != 'clean')
+        throw new RuntimeException( 'Git working copy is not clean' )
+}
+
+
+private String getStatus( dir ) {
+
+    def status = ''
+    process = "git status".execute( null, new File( dir ) )
+    process.in.eachLine { line ->
+        if (line.contains( 'nothing to commit' )) {
+            status = 'clean'
+            return
+        }
+        else {
+            status = 'pending changes'
+            return
+        }
+    }
+    status
 }
 
 
--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,4 +1,4 @@
-/*********************************************************************************
+/* *******************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
  This copyrighted software contains confidential and proprietary information of 
  SunGard Higher Education and its subsidiaries. Any use of this software is limited 
@@ -36,135 +36,148 @@ import org.codehaus.groovy.grails.plugins.GrailsPluginUtils
 import org.codehaus.groovy.grails.resolve.IvyDependencyManager
 
 
+includeTargets << grailsScript("_GrailsEvents")
+
 eventPackagePluginEnd = { pluginName ->
-    
-	if (pluginName == "${metadata.'app.name'}") {
-		try {
-			IvyDependencyManager dm = new IvyDependencyManager( "$pluginName", "${plugin.version}" )
-			
-			dm.parseDependencies( 
-			{
-				distribution = {
-			         localRepository = ""
-			         remoteRepository( id:"releases", url:"http://m038083.sungardhe.com:8081/nexus/content/repositories/releases" ) {
-			             authentication  username:'admin', password:'admin123'
-			         }
-			    }
 
-			    repositories {
-			        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/releases/"
-			        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots/"
-			        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/thirdparty/"
+    if (pluginName == "${metadata.'app.name'}") {
+        try {
+            event "TemplateZip", ["$pluginName", "${plugin.version}"]
 
-			        grailsPlugins()
-			        grailsHome()
-			        grailsCentral()
+            ant.zip( destfile:"${basedir}/grails-${pluginName}-${plugin.version}.zip", 
+            update:true, basedir:"${projectWorkDir}", includes:"template.zip")
 
-			        mavenCentral()
-			        mavenRepo "http://repository.jboss.org/maven2/"
-			        mavenRepo "http://repository.codehaus.org"
-			    }
-			    
-			    dependencies {
-					runtime( 'sungardhe:installerframework:1.0.1' ) {
-						export = false
-					}
-					runtime( 'sungardhe:SGHECommonUtil:1.0.1') {
-						export = false
-					}
-					runtime( 'org.springframework:spring-core:3.0.5.RELEASE') {
-						export = false
-					}
-					runtime( 'org.springframework:spring-expression:3.0.5.RELEASE') {
-						export = false
-					}
-					runtime( 'org.springframework:spring-context:3.0.5.RELEASE') {
-						export = false
-					}
-					runtime( 'org.springframework:spring-beans:3.0.5.RELEASE') {
-						export = false
-					}
-					runtime( 'org.springframework:spring-aop:3.0.5.RELEASE') {
-						export = false
-					}
-					runtime( 'org.springframework:spring-asm:3.0.5.RELEASE') {
-						export = false
-					}
-					runtime( 'org.codehaus.groovy:groovy:1.8.0') {
-						export = false
-					}
-					runtime( 'log4j:log4j:1.2.16') {
-						export = false
-					}
-					runtime( 'commons-logging:commons-logging:1.1.1') {
-						export = false
-					}
-					runtime( 'tomcat:catalina-ant:5.5.23') {
-						export = false
-					}
-			    }
-			} )
-		
-	        ResolveReport report = dm.resolveDependencies()
-	        if (dm.resolveErrors) {
-	            println "Error: There was an error resolving dependencies"
-	            exit 1
-	        }
-					
-			def templateZip  = "$projectWorkDir/template.zip"
-			def stagingDir   = "$projectWorkDir/staging"
-			def installerDir = "$stagingDir/installer"
-			
-			ant.delete( dir:"$stagingDir" )
-			ant.mkdir(  dir:"$stagingDir" )
-			
-			def libDir = "$installerDir/lib"
-			ant.mkdir( dir:"$libDir" )
-			ant.copy( todir:"$libDir", overwrite:true, preservelastmodified:true ) {
-		        if (report) {
-					report.allArtifactsReports.each() {
-						def file = it.localFile
-			            fileset( dir:file.parentFile, includes:file.name )
-					}
-		        }
-			}
-			def instanceDir = "$stagingDir/instance"
-			
-			ant.mkdir( dir:"$instanceDir/i18n" )
-			ant.mkdir( dir:"$instanceDir/css" )
-			ant.mkdir( dir:"$instanceDir/js" )						
-			ant.mkdir( dir:"$instanceDir/config" )
-			
-			File instanceProperties = new File( "$instanceDir/config/instance.properties" )
-			instanceProperties << "shared.config.dir="
-
-			def installerSourceDir = "${basedir}/src/installer"
-			ant.copy( todir:installerDir ) {
-				fileset( dir:installerSourceDir, includes:"**/*" /* excludes:"apache-ant*" */ )
-			}
-		
-			ant.unzip( src:"${basedir}/src/installer/apache-ant-1.8.2-bin.zip", dest:installerDir ) {
-			    patternset {
-        	        exclude( name:"apache-ant-1.8.2/docs/**/*" )
-        	    }
-			}
-		
-		    ant.zip( destfile:templateZip ) {
-				fileset( dir:stagingDir )
-			}
-		
-			ant.zip( destfile:"${basedir}/grails-${pluginName}-${plugin.version}.zip", 
-			         update:true, basedir:"${projectWorkDir}", includes:"template.zip")
-			         
-			ant.delete( dir:stagingDir )
-			ant.delete( file:templateZip )
-		} 
-		catch (Throwable t) {
-		    ant.echo "Caught $t"
-			t.printStackTrace()
-			throw t
-		}
-	}
-		
+            def stagingDir   = "$projectWorkDir/staging"
+            def templateZip  = "$projectWorkDir/template.zip"
+            ant.delete( dir:stagingDir )
+            ant.delete( file:templateZip )
+        }
+        catch (Throwable t) {
+            ant.echo "Caught $t"
+            t.printStackTrace()
+            throw t
+        }
+    }
 }
 
+
+eventTemplateZip = { pluginName, pluginVersion ->
+
+    try {
+        IvyDependencyManager dm = new IvyDependencyManager( "$pluginName", "$pluginVersion" )
+
+        dm.parseDependencies({
+            repositories {
+                mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/releases/"
+                mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots/"
+                mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/thirdparty/"
+                grailsPlugins()
+                grailsHome()
+                grailsCentral()
+                mavenCentral()
+                mavenRepo "http://repository.jboss.org/maven2/"
+                mavenRepo "http://repository.codehaus.org"
+            }
+
+            dependencies {
+                runtime( 'sungardhe:installerframework:1.0.1' ) {
+                    export = false
+                }
+                runtime( 'sungardhe:SGHECommonUtil:1.0.1') {
+                    export = false
+                }
+                runtime( 'org.springframework:spring-core:3.0.5.RELEASE') {
+                    export = false
+                }
+                runtime( 'org.springframework:spring-expression:3.0.5.RELEASE') {
+                    export = false
+                }
+                runtime( 'org.springframework:spring-context:3.0.5.RELEASE') {
+                    export = false
+                }
+                runtime( 'org.springframework:spring-beans:3.0.5.RELEASE') {
+                    export = false
+                }
+                runtime( 'org.springframework:spring-aop:3.0.5.RELEASE') {
+                    export = false
+                }
+                runtime( 'org.springframework:spring-asm:3.0.5.RELEASE') {
+                    export = false
+                }
+                runtime( 'org.codehaus.groovy:groovy:1.8.0') {
+                    export = false
+                }
+                runtime( 'log4j:log4j:1.2.16') {
+                    export = false
+                }
+                runtime( 'commons-logging:commons-logging:1.1.1') {
+                    export = false
+                }
+                runtime( 'tomcat:catalina-ant:5.5.23') {
+                    export = false
+                }
+            }
+        })
+
+        ResolveReport report = dm.resolveDependencies()
+        if (dm.resolveErrors) {
+            println "Error: There was an error resolving dependencies"
+            exit 1
+        }
+
+        def templateZip 
+        pluginSettings.getPluginInfos().each() {
+            if (it.name.equals( "banner-packaging" )) {
+                templateZip = new File( it.pluginDir.getFile(), "template.zip" )
+            }
+        }
+
+        def stagingDir   = "$projectWorkDir/staging"
+        def installerDir = "$stagingDir/installer"
+
+        ant.delete( dir:"$stagingDir" )
+        ant.mkdir(  dir:"$stagingDir" )
+
+        def libDir = "$installerDir/lib"
+        ant.mkdir( dir:"$libDir" )
+        ant.copy( todir:"$libDir", overwrite:true, preservelastmodified:true ) {
+            if (report) {
+               report.allArtifactsReports.each() {
+                   def file = it.localFile
+                   fileset( dir:file.parentFile, includes:file.name )
+               }
+            }
+        }
+
+        def instanceDir = "$stagingDir/instance"
+        ant.mkdir( dir:"$instanceDir/i18n" )
+        ant.mkdir( dir:"$instanceDir/css" )
+        ant.mkdir( dir:"$instanceDir/js" )
+        ant.mkdir( dir:"$instanceDir/config" )
+
+        File instanceProperties = new File( "$instanceDir/config/instance.properties" )
+        instanceProperties << "shared.config.dir="
+
+        // TODO: determine the path to the src/installer, versus assuming it's in-place
+//       def installerSourceDir = "${basedir}/plugins/banner_packaging.git/src/installer"
+        def installerSourceDir = "${basedir}/src/installer"
+        ant.copy( todir:installerDir ) {
+            fileset( dir:installerSourceDir, includes:"**/*" /* excludes:"apache-ant*" */ )
+        }
+
+        ant.unzip( src:"${installerSourceDir}/apache-ant-1.8.2-bin.zip", dest:installerDir ) {
+            patternset {
+                exclude( name:"apache-ant-1.8.2/docs/**/*" )
+            }
+        }
+
+        ant.zip( destfile:templateZip ) {
+            fileset( dir:stagingDir )
+        }
+    }
+    catch (Throwable t) {
+        ant.echo "Caught $t"
+        t.printStackTrace()
+        throw t
+    }
+}
