a72c55f73e470836e96e1e1b345038bd5826d6d2
J.Pack <janice.pack@ellucian.com>
2014-09-22 12:24:54 -0400

    Update config files

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,14 +1,6 @@
-/*********************************************************************************
- Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
- Education and the licensee in question. SunGard is either a registered trademark or
- trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
- Education in the U.S.A. and/or other regions and/or countries.
- **********************************************************************************/
+/*****************************************************************************************
+ * Copyright 2009 - 2014 Ellucian Company L.P. and its affiliates.                       *
+ *****************************************************************************************/
 
  grails.doc.authors = '''Prepared by: SunGard Higher Education
                          |4 Country View Road Malvern, Pennsylvania 19355 United States of America (800) 522 - 4827'''.stripMargin()
@@ -35,7 +27,9 @@
  grails.doc.alias.war           = "2.3 Re-generating the War File"
  grails.doc.alias.deploytomcat  = "2.4 Deploying the War to Tomcat"
 
- 
+
+grails.databinding.useSpringBinder=true
+
 // configuration for plugin testing - will not be included in the plugin zip 
 log4j = {
 
