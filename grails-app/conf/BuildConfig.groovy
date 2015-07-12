a57af0aec93282802fd2a1de49ecc6f4a89d2686
RajanandPK <rajanandppk@ellucian.com>
2015-07-12 12:26:41 +0530

    No JIRA: Lowered dependency resolution log level to error

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,7 +11,7 @@ grails.project.dependency.resolver = "ivy" // or maven
 grails.project.dependency.resolution = {
 	
     inherits("global") { }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 
     distribution = {
     }
