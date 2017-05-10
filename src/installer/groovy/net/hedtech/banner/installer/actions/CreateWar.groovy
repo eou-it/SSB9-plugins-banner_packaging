5c301be66a55320b1a87144fca1f66b745d2b95d
Harshal Bramhankar <Harshal.Bramhankar@ellucian.com>
2017-05-10 10:39:33 +0530

    HRU-7207 Modifying JNDI Name of bannerssbdatasource does not affect in web.xml

--- src/installer/groovy/net/hedtech/banner/installer/actions/CreateWar.groovy
+++ src/installer/groovy/net/hedtech/banner/installer/actions/CreateWar.groovy
@@ -1,14 +1,6 @@
-/* *******************************************************************************
- Copyright 2009-2012 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
- Education and the licensee in question. SunGard is either a registered trademark or
- trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
- Education in the U.S.A. and/or other regions and/or countries.
- **********************************************************************************/
+/*******************************************************************************
+Copyright 2009-2017 Ellucian Company L.P. and its affiliates.
+*******************************************************************************/
 package net.hedtech.banner.installer.actions
 
 import net.hedtech.banner.installer.*
@@ -149,7 +141,7 @@ public class CreateWar extends BaseSystoolAction {
         updateJndi( config.bannerSsbDataSource.jndiName, 
                     'config.bannerSsbDataSource.jndiName', 
                     root, 
-                    'Banner Self Service Datasource', 
+                    'BannerSelfService Datasource', 
                     false )
     }
 
