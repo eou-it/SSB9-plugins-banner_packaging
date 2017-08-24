27181825e1af8f5ff59ec8e42dc436fb3b71e28b
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-08-24 15:05:57 +0530

    code refractoring and property file reading fixed HRU_7358

--- src/installer/groovy/net/hedtech/banner/installer/actions/SamlSetup.groovy
+++ src/installer/groovy/net/hedtech/banner/installer/actions/SamlSetup.groovy
@@ -1,10 +1,10 @@
-/* *******************************************************************************
-Ellucian 2017 copyright
- **********************************************************************************/
+/*******************************************************************************
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package net.hedtech.banner.installer.actions
-import net.hedtech.banner.installer.FileStructure
 
 import com.sungardhe.commoncomponents.installer.ActionRunnerException
+import net.hedtech.banner.installer.FileStructure
 
 /**
  * Installer action for saml setup
@@ -21,7 +21,12 @@ public class SamlSetup extends BaseSystoolAction {
     public void execute() throws ActionRunnerException {
         def currentDir = System.getProperty("user.dir");
         sharedConfigDir = getSharedConfiguration()
-        def config = new ConfigSlurper().parse(resolveFile("${sharedConfigDir.getAbsolutePath()}/saml_configuration.properties").toURL())
+        Properties config = new Properties()
+        File propertiesFile = new File("${sharedConfigDir.getAbsolutePath()}/saml_configuration.properties")
+        propertiesFile.withInputStream {
+            config.load(it)
+        }
+        //def config = new ConfigSlurper().parse(resolveFile("${sharedConfigDir.getAbsolutePath()}/saml_configuration.properties").toURL())
         def alias = config?.getProperty("alias")
         def CN = config?.getProperty("CN")
         def OU = config?.getProperty("OU")
@@ -29,32 +34,34 @@ public class SamlSetup extends BaseSystoolAction {
         def L = config?.getProperty("L")
         def S = config?.getProperty("S")
         def C = config?.getProperty("C")
-        def password1 = config?.getProperty("password")
-        def keystoreName = config?.getProperty("keystoreName")
+        def serviceAssertionLocation = config?.getProperty("serviceAssertionLocation")
         def serviceProviderLocation = config?.getProperty("serviceProviderLocation")
         def identityProviderLocation = config?.getProperty("identityProviderLocation")
+        def certificateAvailable = config?.getProperty("certificateAvailable")
+        def password1 = config?.getProperty("password")
+        def keystoreName = config?.getProperty("keystoreName")
+        println "certificateAvailable " + certificateAvailable
         """keytool -genkey -noprompt -alias $alias -dname "CN=$CN, OU=$OU, O=$O, L=$L, S=$S, C=$C" -keystore $keystoreName -storepass $password1 -keypass $password1""".execute()
+        println "Keystore $keystoreName successfully generated!"
         "keytool -export -alias $alias -storepass $password1 -file ${FileStructure?.INSTANCE_CONFIG_DIR}/SERVICE-PROVIDER.cer -keystore $keystoreName".execute()
-        def ant = new AntBuilder()
-        ant.move( file:"${currentDir}/$keystoreName", todir: "${FileStructure?.INSTANCE_CONFIG_DIR}")
-        def certCommand = "keytool -printcert -rfc -file SERVICE-PROVIDER.cer"
+        println "SERVICE-PROVIDER.cer successfully generated!!!"
+        def certCommand = "keytool -printcert -rfc -file ${FileStructure?.INSTANCE_CONFIG_DIR}/SERVICE-PROVIDER.cer"
         Process certquantCmd = certCommand.execute()
         def certOutput = certquantCmd.in.text
         certOutput = certOutput.replace("-----BEGIN CERTIFICATE-----", "")
         certOutput = certOutput.replace("-----END CERTIFICATE-----", "")
         certOutput = certOutput.trim()
-        generateSPXML(alias, serviceProviderLocation)
-        generateIDPXML(alias, identityProviderLocation)
+        generateSPXML(alias, serviceProviderLocation, serviceAssertionLocation)
+        generateIDPXML(identityProviderLocation)
+        def ant = new AntBuilder()
         certificateInsertion(certOutput)
-        //def config1 = new ConfigSlurper().parse(resolveFile("${FileStructure?.INSTANCE_CONFIG_DIR}/PlatformSandboxApp_configuration.groovy").toURL())
-        //println config1?.banner?.sso?.authenticationProvider
-        //config1?.banner?.sso?.authenticationProvider = "default"
-        applicationConfigChanges(keystoreName,password1,alias)
+        applicationConfigChanges(keystoreName, password1, alias)
+        ant.move(file: "${currentDir}/$keystoreName", todir: "${FileStructure?.INSTANCE_CONFIG_DIR}")
     }
 
     //----------- Private Method ------------------------
 
-    private generateSPXML(alias, location) {
+    private generateSPXML(alias, location, serviceAssertionLocation) {
 
         def content = """|<?xml version="1.0" encoding="UTF-8"?>
                      |  <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" ID="$alias" entityID="$alias">
@@ -84,9 +91,9 @@ public class SamlSetup extends BaseSystoolAction {
                      |      <md:NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:persistent</md:NameIDFormat>
                      |      <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified</md:NameIDFormat>
                      |      <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName</md:NameIDFormat>
-                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="$location" index="0" isDefault="true"/>
-                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser" Location="$location" hoksso:ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact" index="1" xmlns:hoksso="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser"/>
-                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser" Location="$location" hoksso:ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" index="2" xmlns:hoksso="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser"/>
+                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="$serviceAssertionLocation" index="0" isDefault="true"/>
+                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser" Location="$serviceAssertionLocation" hoksso:ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact" index="1" xmlns:hoksso="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser"/>
+                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser" Location="$serviceAssertionLocation" hoksso:ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" index="2" xmlns:hoksso="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser"/>
                      |    </md:SPSSODescriptor>
                      |  </md:EntityDescriptor>
                      |""".stripMargin()
@@ -96,10 +103,10 @@ public class SamlSetup extends BaseSystoolAction {
     }
 
 
-    private generateIDPXML(alias, location) {
+    private generateIDPXML(location) {
 
         def content = """| <?xml version="1.0"?>
-                         |  <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="$alias" cacheDuration="PT1440M">
+                         |  <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="$location" cacheDuration="PT1440M">
                          |      <md:IDPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
                          |          <md:KeyDescriptor use="signing">
                          |              <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
@@ -141,18 +148,18 @@ public class SamlSetup extends BaseSystoolAction {
         ant.replace(file: "${FileStructure?.INSTANCE_CONFIG_DIR}/ServiceProvider.xml", token: "Enter the certificate here!!", value: "$certificateFile")
     }
 
-    private applicationConfigChanges(keystoreName,password1,alias){
+    private applicationConfigChanges(keystoreName, password1, alias) {
         def currentDir = System.getProperty("user.dir")
-        def content ="""|
+        def content = """|
                         |grails.plugin.springsecurity.saml.active = true
                         |grails.plugin.springsecurity.auth.loginFormUrl = '/saml/login'
                         |grails.plugin.springsecurity.saml.afterLogoutUrl ='/logout/customLogout'
-                        |banner.sso.authentication.saml.localLogout='false'                 // To disable single logout set this to true,default 'false'.
-                        |grails.plugin.springsecurity.saml.keyManager.storeFile = 'classpath:security/$keystoreName'  // for unix file based Example:- 'file:/home/u02/samlkeystore.jks'
+                        |banner.sso.authentication.saml.localLogout='false'                                             // To disable single logout set this to true,default 'false'.
+                        |grails.plugin.springsecurity.saml.keyManager.storeFile = 'classpath:security/$keystoreName'    // for unix file based Example:- 'file:/home/u02/samlkeystore.jks'
                         |grails.plugin.springsecurity.saml.keyManager.storePass = '$password1'
                         |grails.plugin.springsecurity.saml.keyManager.passwords = [ '$alias': '$password1' ]
                         |grails.plugin.springsecurity.saml.keyManager.defaultKey = '$alias'
-                        |grails.plugin.springsecurity.saml.metadata.sp.file = 'classpath:security/ServiceProvider.xml'    // for unix file based Example:-'/home/u02/sp-local.xml'
+                        |grails.plugin.springsecurity.saml.metadata.sp.file = 'classpath:security/ServiceProvider.xml'              // for unix file based Example:-'/home/u02/sp-local.xml'
                         |grails.plugin.springsecurity.saml.metadata.providers = [adfs: 'classpath:security/IdentityProvider.xml'] // for unix file based Ex:- '/home/u02/idp-local.xml'
                         |grails.plugin.springsecurity.saml.metadata.defaultIdp = 'adfs'
                         |grails.plugin.springsecurity.saml.metadata.sp.defaults = [
