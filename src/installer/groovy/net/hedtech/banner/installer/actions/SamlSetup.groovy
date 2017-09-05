66b9612f61fa41945b3781e8bca72580c868599a
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-09-05 14:36:36 +0530

    changes for saml automation part of JIRA HRU-7403

--- src/installer/groovy/net/hedtech/banner/installer/actions/SamlSetup.groovy
+++ src/installer/groovy/net/hedtech/banner/installer/actions/SamlSetup.groovy
@@ -4,15 +4,21 @@ Ellucian 2017 copyright
 package net.hedtech.banner.installer.actions
 
 import com.sungardhe.commoncomponents.installer.ActionRunnerException
+import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.XmlUtil
 import net.hedtech.banner.installer.FileStructure
-import java.sql.*
+
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
 
 /**
  * Installer action for saml setup
  **/
 
 public class SamlSetup extends BaseSystoolAction {
-    private File sharedConfigDir
+    private static File sharedConfigDir
     private static final String ALIAS = 'alias'
     private static final String SPASSERTION_LOCATION = 'SPAssertionLocation'
     private static final String SPLOGOUT_LOCATION = 'SPLogoutLocation'
@@ -24,23 +30,22 @@ public class SamlSetup extends BaseSystoolAction {
 
 
     public void execute() throws ActionRunnerException {
-        def alias,SPAssertionLocation,SPLogoutLocation,IDPLocation
+        def alias, SPAssertionLocation, SPLogoutLocation, IDPLocation
         sharedConfigDir = getSharedConfiguration()
         Properties config = new Properties()
         File propertiesFile = new File("${sharedConfigDir.getAbsolutePath()}/saml_configuration.properties")
         propertiesFile.withInputStream {
             config.load(it)
         }
-        def appId= config?.getProperty("appId")
-        def spCertificatePath=config?.getProperty("spCertificatePath")
-        def idpCertificatePath=config?.getProperty("idpCertificatePath")
-        def spXmlPath=config?.getProperty("spXmlPath")
-        def idpXmlPath=config?.getProperty("idpXmlPath")
+        def appId = config?.getProperty("appId")
+        def appName = config?.getProperty("appName")
+        def spCertificatePath = config?.getProperty("spCertificatePath")
+        def idpCertificatePath = config?.getProperty("idpCertificatePath")
+        def spXmlPath = config?.getProperty("spXmlPath")
+        def idpXmlPath = config?.getProperty("idpXmlPath")
         def dbConnection = config?.getProperty("dbconnectionURL")
         def username = config?.getProperty("dbusername")
         def pass = config?.getProperty("dbpassword")
-        def appName = config?.getProperty("appName")
-
         Class.forName("oracle.jdbc.OracleDriver");
         Connection con = DriverManager.getConnection("$dbConnection", "$username", "$pass");
         Statement stmt = con.createStatement();
@@ -75,133 +80,102 @@ public class SamlSetup extends BaseSystoolAction {
         /*"""keytool -genkey -noprompt -alias $alias -dname "CN=$CN, OU=$OU, O=$O, L=$L, S=$S, C=$C" -keystore $keystoreName -storepass $password1 -keypass $password1""".execute()*/
         /*"keytool -export -alias $alias -storepass $password1 -file SERVICE-PROVIDER.cer -keystore $keystoreName"*/
         /*"keytool -export -alias $alias -storepass $password1 -file $currentDir//SERVICE-PROVIDER.cer -keystore $keystoreName".execute()*/
-            def spCertCommand = "keytool -printcert -rfc -file $spCertificatePath"
-            Process spCertquantCmd = spCertCommand.execute()
-            println "***************************************************"
-            def spCertOutput = spCertquantCmd.in.text
-            spCertOutput = spCertOutput.replace("-----BEGIN CERTIFICATE-----", "")
-            spCertOutput = spCertOutput.replace("-----END CERTIFICATE-----", "")
-            spCertOutput = spCertOutput.trim()
-            println spCertOutput
-            generateSPXML(alias, SPLogoutLocation, SPAssertionLocation)
-            certificateInsertion(spCertOutput)
+        def spCertCommand = "keytool -printcert -rfc -file $spCertificatePath"
+        Process spCertquantCmd = spCertCommand.execute()
+        println "***************************************************"
+        def spCertOutput = spCertquantCmd.in.text
+        spCertOutput = spCertOutput.replace("-----BEGIN CERTIFICATE-----", "")
+        spCertOutput = spCertOutput.replace("-----END CERTIFICATE-----", "")
+        spCertOutput = spCertOutput.trim()
+        println spCertOutput
+        updateSPXML(spXmlPath, alias, SPLogoutLocation, SPAssertionLocation, spCertOutput, appName)
+        certificateInsertion(spCertOutput)
 
-            def idpCertCommand = "keytool -printcert -rfc -file $idpCertificatePath"
-            Process idpCertquantCmd = idpCertCommand.execute()
-            println "***************************************************"
-            def idpCertOutput = idpCertquantCmd.in.text
-            idpCertOutput = idpCertOutput.replace("-----BEGIN CERTIFICATE-----", "")
-            idpCertOutput = idpCertOutput.replace("-----END CERTIFICATE-----", "")
-            idpCertOutput = idpCertOutput.trim()
-            println idpCertOutput
-            generateIDPXML(IDPLocation)
-            applicationConfigChanges(appName,alias)
-           // readingSPXML(spXmlPath, alias, SPLogoutLocation, SPAssertionLocation)
-            def ant = new AntBuilder()
-            ant.copy( file:"$idpCertificatePath", todir: "${FileStructure?.INSTANCE_CONFIG_DIR}")
-            ant.copy( file:"$spCertificatePath", todir: "${FileStructure?.INSTANCE_CONFIG_DIR}")
-        }
+        def idpCertCommand = "keytool -printcert -rfc -file $idpCertificatePath"
+        Process idpCertquantCmd = idpCertCommand.execute()
+        println "***************************************************>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+        def idpCertOutput = idpCertquantCmd.in.text
+        idpCertOutput = idpCertOutput.replace("-----BEGIN CERTIFICATE-----", "")
+        idpCertOutput = idpCertOutput.replace("-----END CERTIFICATE-----", "")
+        idpCertOutput = idpCertOutput.trim()
+        println idpCertOutput
+        updateIDPXML(idpXmlPath, IDPLocation, idpCertOutput, appName)
+        applicationConfigChanges(appName, alias)
+        def ant = new AntBuilder()
+        ant.copy(file: "$idpCertificatePath", todir: "${FileStructure?.INSTANCE_CONFIG_DIR}")
+        ant.copy(file: "$spCertificatePath", todir: "${FileStructure?.INSTANCE_CONFIG_DIR}")
+    }
 
-        //----------- Private Method ------------------------
+    /**
+     * updateSPXML method is to read the service-provider xml from the path mentioned in SS config table and
+     * update the corresponding values based on values provided in SS config table
+     * and writing the XML to new file (banner-appname-sp.xml) in sharedConfiguration location
+     * @param path
+     * @param alias
+     * @param SPLogoutLocation
+     * @param SPAssertionLocation
+     * @param spCertOutput
+     * @param appName
+     * @return
+     */
 
-        private generateSPXML(alias, location, SPAssertionLocation) {
+    private updateSPXML(path, alias, SPLogoutLocation, SPAssertionLocation, spCertOutput, appName) {
+        File inputFile = new File("$path")
+        def spXML = new XmlSlurper().parse(inputFile)
+        spXML?.SPSSODescriptor?.KeyDescriptor?.KeyInfo?.X509Data?.X509Certificate?.replaceBody "$spCertOutput"
+        spXML['@ID'] = "$alias"
+        spXML['@entityID'] = "$alias"
+        spXML?.SPSSODescriptor?.SingleLogoutService['@Location'] = "$SPLogoutLocation"
+        spXML?.SPSSODescriptor?.AssertionConsumerService['@Location'] = "$SPAssertionLocation"
 
-            def content = """|<?xml version="1.0" encoding="UTF-8"?>
-                     |  <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" ID="$alias" entityID="$alias">
-                     |    <md:SPSSODescriptor AuthnRequestsSigned="false" WantAssertionsSigned="false" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
-                     |      <md:KeyDescriptor use="signing">
-                     |        <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-                     |          <ds:X509Data>
-                     |            <ds:X509Certificate>
-                     |                  Enter the certificate here!!
-                     |            </ds:X509Certificate>
-                     |          </ds:X509Data>
-                     |        </ds:KeyInfo>
-                     |      </md:KeyDescriptor>
-                     |      <md:KeyDescriptor use="encryption">
-                     |        <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-                     |          <ds:X509Data>
-                     |            <ds:X509Certificate>
-                     |                  Enter the certificate here!!
-                     |            </ds:X509Certificate>
-                     |          </ds:X509Data>
-                     |        </ds:KeyInfo>
-                     |      </md:KeyDescriptor>
-                     |      <md:SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="$location"/>
-                     |      <md:SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="$location"/>
-                     |      <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</md:NameIDFormat>
-                     |      <md:NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:transient</md:NameIDFormat>
-                     |      <md:NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:persistent</md:NameIDFormat>
-                     |      <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified</md:NameIDFormat>
-                     |      <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName</md:NameIDFormat>
-                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="$SPAssertionLocation" index="0" isDefault="true"/>
-                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser" Location="$SPAssertionLocation" hoksso:ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact" index="1" xmlns:hoksso="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser"/>
-                     |      <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser" Location="$SPAssertionLocation" hoksso:ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" index="2" xmlns:hoksso="urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser"/>
-                     |    </md:SPSSODescriptor>
-                     |  </md:EntityDescriptor>
-                     |""".stripMargin()
-            def serviceProviderFile = new File("${FileStructure?.INSTANCE_CONFIG_DIR}/ServiceProvider.xml")
-            serviceProviderFile.write content
-        }
+        def newwriter = new FileWriter("${sharedConfigDir.getAbsolutePath()}/banner-$appName-sp.xml")
+        def result = new StreamingMarkupBuilder().bind { mkp.yield spXML }.toString()
+        new XmlSlurper().parseText(result)
+        XmlUtil.serialize(result, newwriter)
+    }
 
+    /**
+     * updateIDPXML method is to read the identity-provider xml from the path mentioned in SS config table and
+     * update the corresponding values based on values provided in SS config table
+     * and writing the XML to new file (banner-appname-idp.xml) in sharedConfiguration location
+     * @param path
+     * @param IDPLocation
+     * @param idpCertOutput
+     * @param appName
+     * @return
+     */
+    private updateIDPXML(path, IDPLocation, idpCertOutput, appName) {
+        File inputFile = new File("$path")
+        def idpXML = new XmlSlurper().parse(inputFile)
+        idpXML?.SPSSODescriptor?.KeyDescriptor?.KeyInfo?.X509Data?.X509Certificate?.replaceBody "$idpCertOutput"
+        idpXML['@entityID'] = "$IDPLocation"
+        idpXML?.SPSSODescriptor?.SingleLogoutService['@Location'] = "$IDPLocation"
+        idpXML?.SPSSODescriptor?.SingleSignOnService['@Location'] = "$IDPLocation"
 
-        private generateIDPXML(location) {
+        def newwriter = new FileWriter("${sharedConfigDir.getAbsolutePath()}/banner-$appName-idp.xml")
+        def result = new StreamingMarkupBuilder().bind { mkp.yield idpXML }.toString()
+        new XmlSlurper().parseText(result)
+        XmlUtil.serialize(result, newwriter)
+    }
 
-            def content = """| <?xml version="1.0"?>
-                         |  <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="$location" cacheDuration="PT1440M">
-                         |      <md:IDPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
-                         |          <md:KeyDescriptor use="signing">
-                         |              <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-                         |                  <ds:X509Data>
-                         |                      <ds:X509Certificate>
-                         |                          <!-- <Extracted-Data This is the certificate of WSO2 / EIS or Identity Provider server using wso2carbon.jks> -->
-                         |                      </ds:X509Certificate>
-                         |                  </ds:X509Data>
-                         |              </ds:KeyInfo>
-                         |          </md:KeyDescriptor>
-                         |          <md:KeyDescriptor use="encryption">
-                         |              <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-                         |                  <ds:X509Data>
-                         |                      <ds:X509Certificate>
-                         |                          <!-- <Extracted-Data This is the certificate of WSO2 / EIS or Identity Provider server using wso2carbon.jks> -->
-                         |                      </ds:X509Certificate>
-                         |                  </ds:X509Data>
-                         |              </ds:KeyInfo>
-                         |          </md:KeyDescriptor>
-                         |          <md:SingleLogoutService Location="$location" Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"/>
-                         |          <md:SingleLogoutService Location="$location" Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"/>
-                         |          <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:entity</md:NameIDFormat>
-                         |          <md:SingleSignOnService Location="$location" Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"/>
-                         |          <md:SingleSignOnService Location="$location" Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"/>
-                         |      </md:IDPSSODescriptor>
-                         |      <md:ContactPerson contactType="administrative"/>
-                         |  </md:EntityDescriptor>
-                     |""".stripMargin()
-            def currentDir = System.getProperty("user.dir");
-            def identityProviderFile = new File("${FileStructure?.INSTANCE_CONFIG_DIR}/IdentityProvider.xml")
-            identityProviderFile.write content
-
-        }
-
-        private certificateInsertion(certificateFile) {
-            def currentDir = System.getProperty("user.dir")
-            def ant = new AntBuilder()
-            ant.replace(file: "${FileStructure?.INSTANCE_CONFIG_DIR}/ServiceProvider.xml", token: "Enter the certificate here!!", value: "$certificateFile")
-        }
+    private certificateInsertion(certificateFile) {
+        def ant = new AntBuilder()
+        ant.replace(file: "${FileStructure?.INSTANCE_CONFIG_DIR}/ServiceProvider.xml", token: "Enter the certificate here!!", value: "$certificateFile")
+    }
 
 
-        private applicationConfigChanges(keystoreName,alias){
-            def content ="""|
+    private applicationConfigChanges(appName, alias) {
+        def content = """|
                         |grails.plugin.springsecurity.saml.active = true
                         |grails.plugin.springsecurity.auth.loginFormUrl = '/saml/login'
                         |grails.plugin.springsecurity.saml.afterLogoutUrl ='/logout/customLogout'
                         |banner.sso.authentication.saml.localLogout='false'                 // To disable single logout set this to true,default 'false'.
-                        |grails.plugin.springsecurity.saml.keyManager.storeFile = 'classpath:security/$keystoreName'  // for unix file based Example:- 'file:/home/u02/samlkeystore.jks'
+                        |grails.plugin.springsecurity.saml.keyManager.storeFile = 'classpath:security/$appName'  // for unix file based Example:- 'file:/home/u02/samlkeystore.jks'
                         |grails.plugin.springsecurity.saml.keyManager.storePass = '<PASSWORD>'
                         |grails.plugin.springsecurity.saml.keyManager.passwords = [ '$alias': '<PASSWORD>' ]
                         |grails.plugin.springsecurity.saml.keyManager.defaultKey = '$alias'
-                        |grails.plugin.springsecurity.saml.metadata.sp.file = 'classpath:security/ServiceProvider.xml'    // for unix file based Example:-'/home/u02/sp-local.xml'
-                        |grails.plugin.springsecurity.saml.metadata.providers = [adfs: 'classpath:security/IdentityProvider.xml'] // for unix file based Ex:- '/home/u02/idp-local.xml'
+                        |grails.plugin.springsecurity.saml.metadata.sp.file = 'classpath:security/banner-$appName-sp'.xml'    // for unix file based Example:-'/home/u02/sp-local.xml'
+                        |grails.plugin.springsecurity.saml.metadata.providers = [adfs: 'classpath:security/banner-$appName-idp.xml'] // for unix file based Ex:- '/home/u02/idp-local.xml'
                         |grails.plugin.springsecurity.saml.metadata.defaultIdp = 'adfs'
                         |grails.plugin.springsecurity.saml.metadata.sp.defaults = [
                         |       local: true,
@@ -215,7 +189,7 @@ public class SamlSetup extends BaseSystoolAction {
                         |       requireLogoutResponseSigned: false
                         |]
                         |""".stripMargin()
-            def appConfig = new File("${FileStructure?.INSTANCE_CONFIG_DIR}/app_saml_config.txt")
-            appConfig.write content
-        }
+        def appConfig = new File("${FileStructure?.INSTANCE_CONFIG_DIR}/app_saml_config.txt")
+        appConfig.write content
     }
+}
