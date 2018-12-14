70a1f4aaeacb57feb093d6079a9e5a0d6f4f5eb7
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-12-14 13:02:01 +0530

    HRU-8389 taking the plugin script to run and test the packaging plugin changes

--- src/main/scripts/selfservice-packaging.groovy
+++ src/main/scripts/selfservice-packaging.groovy
@@ -1,52 +1,36 @@
-
-String pluginDirs = System.getProperty("user.dir");
-println " pluginDirectory is >>>>"+pluginDirs
+String pluginDirs = System.getProperty("user.dir")
+pluginDirs = pluginDirs+"\\plugins\\banner_packaging.git"
 GroovyClassLoader cLoader = new GroovyClassLoader(this.class.getClassLoader())
 
 def shellCommandPref = this.class.classLoader.parseClass(new File("$pluginDirs/src/main/groovy/net/hedtech/banner/utility/ShellCommandPrefix.groovy"))
-
-
 String shellCommandPrefix = cLoader.loadClass("net.hedtech.banner.utility.ShellCommandPrefix").getShellCommandPrefix()
 
+String pluginDirectory = System.getProperty("user.dir")
+System.setProperty('Dfile.encoding','UTF-8')
+pluginDirectory = pluginDirectory+"\\plugins\\banner_packaging.git"
+int endIndex = pluginDirectory.lastIndexOf("\\");
+String applicationRoot
 
-println '>>>>>>>>>>>>>shell CommandPrefix ::  ' + shellCommandPrefix
-
-String pluginDirectory = System.getProperty("user.dir");
-
-
-println(">>>>>>>>>>>>>>>>>>>>>>>    pluginDirectory   " + pluginDirectory)
-int endIndex = pluginDirectory.lastIndexOf("/");
-println(">>>>>>>>>>>>>>>>>>>>>>>" + endIndex)
-String applicationRoot;
-
-//change directory to top level up
+//change directory to to level up
 if (endIndex != -1) {
-    applicationRoot = pluginDirectory.substring(0, endIndex); // not forgot to put check if(endIndex != -1)
-    endIndex = applicationRoot.lastIndexOf("/");
-    applicationRoot = pluginDirectory.substring(0, endIndex);
-    println '  >>>>>>>>> rootDir  is ' + applicationRoot
+    applicationRoot = pluginDirectory.substring(0, endIndex)     // not forgot to put check if(endIndex != -1)
+    endIndex = applicationRoot.lastIndexOf("\\");
+    applicationRoot = pluginDirectory.substring(0, endIndex)
 }
 def ant = new groovy.util.AntBuilder()
 
-println "**************************************************************************************"
-
-File pluginRoot = new File(applicationRoot + "/plugins");
+File pluginRoot = new File(applicationRoot + "/plugins")
 File[] files = pluginRoot.listFiles();
 for (File file : files) {
     if (file.isDirectory()) {
-        System.out.println("directory:" + file.getCanonicalPath());
-        println "    ......... Deleting contents of ::" + file.getCanonicalPath() + "/build"
-
-        if( new File(file.getCanonicalPath() + "/build").exists()){
-            ant.delete(dir: file.getCanonicalPath() + "/build")
+        if( new File(file.getCanonicalPath() + "\\build").exists()){
+            ant.delete(dir: file.getCanonicalPath() + "\\build")
         }
     }
 }
-if((new File(applicationRoot+"/build")).exists()){
-    ant.delete(dir: applicationRoot+"/build", failonerror: false)
+if((new File(applicationRoot+"\\build")).exists()){
+    ant.delete(dir: applicationRoot+"\\build", failonerror: false)
 }
-
-println "**************************************************************************************"
 
 if (isDirty(applicationRoot, shellCommandPrefix)) {
     println "*******************************************"
@@ -58,13 +42,12 @@ if (isDirty(applicationRoot, shellCommandPrefix)) {
 println ">>>>>>>>> OS is " + System.getProperty('os.name');
 
 if (System.properties.'os.name'.startsWith('Windows')) {
-    println ' >>>os is windows'
     command = "grails.bat"
 
 } else {
     command = "grails"
 }
-println '>>>>>>>>>>>>>>>building WAR'
+
 ant.exec(executable: command, dir: "$applicationRoot", failonerror: true) {
     arg(value: "prod")
     arg(value: "war")
@@ -74,10 +57,9 @@ ant.exec(executable: command, dir: "$applicationRoot", failonerror: true) {
 String warLocation = applicationRoot + "/build/libs/Release"
 File releaseDir = new File(warLocation);
 File[] artifacts = releaseDir.listFiles();
-String appName;
+String appName
 String appVersion;
 for (File file : artifacts) {
-    System.out.println("directory:" + file.getCanonicalPath());
     if (file.getName().endsWith(".war")) {
         StringTokenizer tokens = new StringTokenizer(file.getName(), "-");
         if (tokens.hasMoreTokens()) {
@@ -89,43 +71,31 @@ for (File file : artifacts) {
     }
 }
 
-
-
-
-
 appVersion = appVersion.subSequence(0, appVersion.lastIndexOf('.'))
-println "   >>>>>>>>>>> appName is ::" + appName
-println "   >>>>>>>>>>> appVersion is ::" + appVersion
 
 System.setProperty('appVersion',appVersion);
 
 System.setProperty('appName',appName);
 System.setProperty('applicationRoot', applicationRoot)
-println '>>>>>>>>>>>>>>>Generating release.properties'
-
-
 
 def message = "Application Name: ${appName} ::  Application Version: ${appVersion} "
-println "***************************************************** message ::"+message
-println " >>>>>>>>> renaming application.groovy to application.properties"
-
 
 ant.copy (file: "${applicationRoot}/grails-app/conf/application.groovy", tofile: "${applicationRoot}/build/libs/release/application.properties" )
 
 Properties prop = new Properties();
 String propertiesPath= "${applicationRoot}/build/libs/release/application.properties"
-InputStream input;
+InputStream input
 try{
-    input = new FileInputStream(propertiesPath);
+    input = new FileInputStream(propertiesPath)
     prop.load(input)
 } catch (IOException ex) {
-    ex.printStackTrace();
+    ex.printStackTrace()
 } finally {
     if (input != null) {
         try {
-            input.close();
+            input.close()
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace()
         }
     }
 }
@@ -137,13 +107,7 @@ appId = appId.replace("\"", "")
 def scmRevision         = "$shellCommandPrefix git rev-parse HEAD".execute(null, new File(applicationRoot)).text
 def scmRepository       = "$shellCommandPrefix git config --get remote.origin.url".execute(null, new File(applicationRoot)).text
 
-
 def workingBranchStatus =  getStatus( applicationRoot, shellCommandPrefix )
-
-println "*****************************************************************************************************"
-println "<<< Start Generating release.properties >>>"
-println "*****************************************************************************************************"
-
 
 def content = """|#This file is automatically generated and contains release specific properties.
                          |#This file MUST not be changed.
@@ -179,21 +143,13 @@ def content = """|#This file is automatically generated and contains release spe
                          |#**************************************************** */
                          |""".stripMargin();
 
-
 for (File file : files) {
     if (file.isDirectory()) {
         def fullPath =file.getCanonicalPath()
-        System.out.println("directory:" + file.getCanonicalPath());
-        String pluginName
-		if(System.properties.'os.name'.startsWith('Windows')){
-			pluginName= getPluginName(applicationRoot + "\\plugins\\", fullPath)
-		}else{
-			pluginName= getPluginName(applicationRoot + "/plugins/", fullPath)
-		}
+        String pluginName= getPluginName(applicationRoot + "\\plugins\\", fullPath)
         def treeish = resolvePluginSha1(applicationRoot,pluginName)
         def branch = getWorkingBranch(fullPath,shellCommandPrefix)
         def status = getStatus( fullPath,shellCommandPrefix )
-        println">>>>>>>>>>> plugin name is "+pluginName
         content += "\n#  Plugin         :  ${pluginName}"
         content += "\n#  location       :  ${fullPath}"
         content += "\n#  Git treeish    :  ${treeish}"
@@ -203,13 +159,12 @@ for (File file : files) {
         content += "\n"
     }
 }
-def releasePropertiesFile = new File( "$applicationRoot/build/libs/Release/release.properties" );
-releasePropertiesFile.write content;
+def releasePropertiesFile = new File( "$applicationRoot/build/libs/release.properties" );
+releasePropertiesFile.write content
 
 /**
  * SAML CONFIGURATION FILE GENERATION
  */
-
 def samlConfigContent = """|#This file is automatically generated and contains SAML Configuration specific properties.
                      |#This file MUST be changed when SAML configuration is required
                      |#
@@ -227,48 +182,9 @@ samlConfigurationFile.write samlConfigContent
 
 ant.delete(file: propertiesPath)
 
-println "*****************************************************************************************************"
-println "<<< End  Generating release.properties >>>"
-println "*****************************************************************************************************"
-
-/*****************************************************************************************************/
-
-
-println "*****************************************************************************************************"
-println "<<< Start updating war with  release.properties >>>"
-println "*****************************************************************************************************"
-String repackagePath = applicationRoot + "/build/libs/Re-package/"+appName
-String warName =appName+"-"+appVersion+".war"
-ant.mkdir( dir: repackagePath)
-ant.unzip(src: warLocation+"/"+warName, dest: repackagePath, overwrite: "true")
-
-ant.copy (todir: repackagePath+"/WEB-INF/classes"){
-    fileset( dir:"$applicationRoot/build/libs/Release", includes:"release.properties" )
-}
-
-ant.delete( file: warLocation+"/"+warName )
-
-ant.zip(destfile: warLocation+"/"+warName, basedir: repackagePath, encoding: "UTF-8")
-
-ant.delete( dir: applicationRoot + "/build/libs/Re-package/" )
-println "*****************************************************************************************************"
-println "<<< Completed updating  release.properties >>>"
-println "*****************************************************************************************************"
-
-
-
-
-
-
-/*****************************************************************************************************/
-
-println "*****************************************************************************************************"
-println "<<< Preparing to   Generating "+ warLocation + "/" + appName + "-" + appVersion + ".zip" +" file >>>"
-println "*****************************************************************************************************"
 def projectWorkDir = warLocation + "/" + appName
 File releasePackageZip = new File(warLocation + "/" + appName + "-" + appVersion + ".zip")
 
-println '>>>>>>>>>>>>>>>Generating deleting releasePackageZip'
 ant.delete(file: releasePackageZip)
 def configDir = projectWorkDir+"/config"
 def i18nDir = projectWorkDir+"/i18n"
@@ -277,41 +193,26 @@ def instanceDir = projectWorkDir+"/instance"
 def libDir= projectWorkDir+"/lib"
 def webapp= projectWorkDir+"/webapp"
 
-
-
-println '>>>>>>>>>>>>>>>>>>>>>>>>>projectWorkDir is ::' + projectWorkDir
-//ant.delete(dir: stagingDir)
-println '>>>>>>>>>>>>>>>>>>>>>>>>>creating  ::' + configDir
 ant.mkdir(dir: configDir)
-println '>>>>>>>>>>>>>>>>>>>>>>>>>creating  ::' + i18nDir
 ant.mkdir(dir: i18nDir)
-println '>>>>>>>>>>>>>>>>>>>>>>>>>creating  ::' + installerDir
 ant.mkdir(dir: installerDir)
-println '>>>>>>>>>>>>>>>>>>>>>>>>>creating  ::' + instanceDir
 ant.mkdir(dir: instanceDir)
-println '>>>>>>>>>>>>>>>>>>>>>>>>>creating  ::' + libDir
 ant.mkdir(dir: libDir)
-println '>>>>>>>>>>>>>>>>>>>>>>>>>creating  ::' + webapp
 ant.mkdir(dir: webapp)
+ant.mkdir( dir:"$installerDir/lib" )
 
-println '>>>>>>>>>>>>>>>>>>>>>>>>>Copying Cofiguration files  ::'
 ant.copy (todir: configDir){
     fileset( dir:"${applicationRoot}", includes:"*.example" )
+    fileset( dir:"$applicationRoot/build/libs", includes:"saml_configuration.properties" )
 }
 
-ant.copy(todir: configDir) {
-    fileset( file:"$applicationRoot/build/libs/saml_configuration.properties" )
-}
-
-println '>>>>>>>>>>>>>>>>>>>>>>>>>Copying i18n files  ::' + webapp
 ant.copy (todir: i18nDir){
     fileset( dir:"${applicationRoot}/grails-app/i18n", includes:"*.properties" )
 }
 ant.copy (todir: i18nDir){
-    fileset( file:"$applicationRoot/build/libs/Release/release.properties" )
+    fileset( dir:"$applicationRoot/build/libs", includes:"release.properties" )
+    fileset( file:"$applicationRoot/build/libs/release.properties" )
 }
-
-
 
 File customDir = new File( "${pluginDirectory}/src/main/installer" )
 if (customDir.exists()) {
@@ -326,7 +227,6 @@ ant.mkdir( dir:"$instanceDir/js" )
 ant.mkdir( dir:"$instanceDir/images" )
 ant.mkdir( dir:"$instanceDir/config" )
 
-
 File instanceProperties = new File( "$instanceDir/config/instance.properties" )
 instanceProperties << "shared.config.dir="
 ant.copy (todir: libDir){
@@ -338,7 +238,13 @@ ant.copy (todir: webapp){
 }
 ant.copy( todir: "$installerDir/lib", overwrite:true, preservelastmodified:true ) {
     fileset( dir: "${pluginDirectory}/lib", includes:"*.jar" )
+    fileset( dir:"${warLocation}", includes:"ojdbc*.jar" )
+    fileset( dir:"${warLocation}", includes:"xdb*.jar" )
 }
+
+ant.delete(file: "${applicationRoot}/build/libs/release.properties")
+ant.delete(file: "${applicationRoot}/build/libs/saml_configuration.properties")
+
 ant.unzip(src: "${pluginDirectory}/src/main/installer/apache-ant-1.8.2-bin.zip", dest: installerDir, overwrite: "true")
 ant.zip(destfile: warLocation + "/release-" + appName + "-" + appVersion + ".zip", basedir: projectWorkDir)
 
@@ -354,9 +260,6 @@ ant.zip( destFile: pluginDirectory+"/build/libs/template.zip") {
 }
 
 ant.delete( dir: projectWorkDir )
-println "*****************************************************************************************************"
-println "<<< Completed  Generating "+ warLocation + "/" + appName + "-" + appVersion + ".zip" +" file >>>"
-println "*****************************************************************************************************"
 
 private boolean isDirty(dir, shellCommandPrefix) {
     if (getStatus(dir, shellCommandPrefix) != 'clean')
@@ -368,9 +271,7 @@ private boolean isDirty(dir, shellCommandPrefix) {
 private String getStatus(dir, shellCommandPrefix) {
 
     def status = 'not clean (changes found)'
-    println '>>>>>>>>>>>>>shellCommandPrefix' + shellCommandPrefix
-    println">>>>>>>>>>>>>> dir is "+dir
-    process = "$shellCommandPrefix git status".execute(null,new File(dir))
+    process = "$shellCommandPrefix git status".execute(null, new File(dir))
     process.in.eachLine { line ->
         if (line.contains('nothing to commit')) {
             status = 'clean'
@@ -381,25 +282,16 @@ private String getStatus(dir, shellCommandPrefix) {
 }
 
 private String getPluginName(applicationRoot, fullPath){
-    println ">>>>>>>>>>>fullPath is ::"+fullPath
-
     String pluginName = fullPath.replace(applicationRoot, "")
-    println ">>>>>>>>>>>> Plugin Name is ::"+pluginName
     return pluginName
-
 }
 
 private String resolvePluginSha1( dir, pluginName ) {
     def gitDir = new File( "${dir}/.git" )
-    try{
-        if ( gitDir.isDirectory() ) {
-            new File("${dir}/.git/modules/plugins/${pluginName}/refs/heads/master").text
-        } else {
-            ""
-        }
-    }catch(FileNotFoundException fne){
-        println " Directory ::"+"${dir}/.git/modules/plugins/${pluginName}/refs/heads/master" +"::  Missing"
-        return ""
+    if ( gitDir.isDirectory() ) {
+        new File("${dir}/.git/modules/plugins/${pluginName}/refs/heads/master").text
+    } else {
+        ""
     }
 }
 
