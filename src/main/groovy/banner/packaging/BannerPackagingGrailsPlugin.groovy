0186f91df038817d72cedecee4615870faf612d0
chirayuh <Chirayu.H@ellucian.com>
2020-04-23 10:36:55 +0530

    HRU-9476: Update grails version in BannerPackagingGrailsPlugin.groovy file.

--- src/main/groovy/banner/packaging/BannerPackagingGrailsPlugin.groovy
+++ src/main/groovy/banner/packaging/BannerPackagingGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import grails.plugins.*
 class BannerPackagingGrailsPlugin extends Plugin {
     def version = "9.18.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.2 > *"
+    def grailsVersion = "3.3.11 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
