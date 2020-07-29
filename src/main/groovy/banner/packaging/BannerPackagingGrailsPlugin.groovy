ff6bfdc321ba2f1405238ca2beb8f9d87e023235
Soven Rout <sovenr@ellucian.com>
2020-07-29 17:53:31 +0530

    HRU-9671: Removed unnecessary changed.

--- src/main/groovy/banner/packaging/BannerPackagingGrailsPlugin.groovy
+++ src/main/groovy/banner/packaging/BannerPackagingGrailsPlugin.groovy
@@ -2,8 +2,6 @@
  Copyright 2020 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
-
-/**
 package banner.packaging
 
 import grails.plugins.*
