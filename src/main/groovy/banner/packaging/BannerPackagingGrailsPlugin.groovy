86ea91437116809cb7bac1adcb8b7d9df7228bde
chirayuh <Chirayu.H@ellucian.com>
2020-08-04 18:40:38 +0530

    HRU-9671: Revert changes.

--- src/main/groovy/banner/packaging/BannerPackagingGrailsPlugin.groovy
+++ src/main/groovy/banner/packaging/BannerPackagingGrailsPlugin.groovy
@@ -2,6 +2,8 @@
  Copyright 2020 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
+
+/**
 package banner.packaging
 
 import grails.plugins.*
